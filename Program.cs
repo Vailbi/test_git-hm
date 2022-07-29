5fae2f15868cb118999bfb5e19d053e7fdef7312
Fedor <vailbi94@gmail.com>
2022-07-29 23:19:19 +0300

    add print array

--- Program.cs
+++ Program.cs
@@ -19,3 +19,21 @@ for (int i = 0; i < inputArr.Length; i++)
         j++;
     }
 }
+string Print(string[] array)
+{
+    int length = array.Length;
+    string output = String.Empty;
+    for (int i = 0; i < length; i++)
+    {
+        if (i == length-1)
+        {
+            output+= ($"\"{array[i]}\"");
+        }
+        else
+        output+= ($"\"{array[i]}\", ");
+    }
+    return output;
+}
+
+Console.WriteLine($"[{Print(newArr)}]");
+File.WriteAllText("output.txt" , Print(newArr));
